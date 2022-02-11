74a5dcf250b31bd732ce62cca9eee63ad40c33e9
cypher <cyphersept@protonmail.comm>
2022-02-10 19:41:02 -0500

    Completed Etch-a-Sketch

--- rps-script.js
+++ rps-script.js
@@ -3,33 +3,41 @@
 const rock = {
     name:"Rock",
     upper:this.name.toUpperCase(),
-    button:document.querySelector('.rock'),
+    button:document.querySelector('.left'),
+    emoji:"✊",
     weakness:paper
 }
 
 const paper = {
     name:"Paper",
     upper:this.name.toUpperCase(),
-    button:document.querySelector('.paper'),
+    button:document.querySelector('.mid'),
+    emoji:"🖐",
     weakness:scissors
 }
 
 const scissors = {
     name:"Scissors",
     upper:this.name.toUpperCase(),
-    button:document.querySelector('.scissors'),
+    button:document.querySelector('.right'),
+    emoji:"✌",
     weakness:rock
 }
 
 const score = {
     playerPts:0,
     cpuPts:0,
-    playerWins:0,
-    cpuWins:0
+}
+
+const display = {
+    play: document.querySelector('.rps .play'),
+    msg: document.querySelector('.rps .msg'),
+    player: document.querySelector('.rps .player'),
+    cpu: document.querySelector('.rps .cpu')
 }
 
 const options = [rock, paper, scissors];
-let selection = options[0];
+let selection //selected button
 
 
 options.forEach(item => {
@@ -38,6 +46,8 @@ options.forEach(item => {
     })
 })
 
+
+
 //https://www.delftstack.com/howto/javascript/javascript-toggle-button/
 
 //Refactor into literal "mini" games contained in boxes, several visible on screen at once to minimize clicking
@@ -45,14 +55,15 @@ options.forEach(item => {
 function computerPlay() {    
     const num = Math.floor(Math.random() * 3); //Number from 0-2
     const result = options[num];
-    console.log(`CPU chose ${result.name}!`);
+    updateText(`CPU chose ${result.name}!`);
     return result;
 }
 
-function playerPlay(type) {    
-    let result = selection;
-    if (type == "console") result = playerInput("Rock, paper, or scissors?"); //repeats until valid
-    console.log(`You chose ${result.name}!`);
+function playerPlay(mode) {    
+    let result;
+    if (mode == "UI") result = selection;
+    else result = playerInput("Rock, paper, or scissors?"); //repeats until valid
+    updateText(`You chose ${result.name}!`);
     return result;
 }
 
@@ -70,10 +81,11 @@ function validateInput(input) { //Returns index of valid input OR undefined
     return result;
 }
 
-function playRound(type) {
-    let player = playerPlay(type);
-    let cpu = computerPlay();
-    return pickWinner(player, cpu);
+function playRound(mode) {
+    const player = playerPlay(mode);
+    const cpu = computerPlay();
+    const winner = pickWinner(player, cpu);
+    updateText(winner);
     /*let winner = "";
     if (player == cpu) winner = "Tie";
         else if ((player == cpu + 1) || (player == 0) && (cpu == 2)) winner = "Player";
@@ -90,31 +102,41 @@ function pickWinner(player, cpu) {
         text = "I win the round!"
         score.cpuPts++;
     }
+    return text;
 }
 
-function playGameText() {
-    const player = score.playerPts;
-    const cpu = score.cpuPts
+function playGame(mode) {   
+
+    if (mode == "UI") while (player < 5 && cpu < 5) playRound(UI);
+    else for (let i = 0; i < 5; i++) playRound();
     
-    for (let i = 0; i < 5; i++) playRound("console");    
-    if (player < cpu) console.log("[Congratulations! You're the winner!]");
-    else if (player < cpu) console.log("[The CPU is the winner. Better luck next time!]");
-    else console.log("[It's a tie! Play again?]");
+    let text = "[It's a tie! Play again?]";
+    if (score.playerPts > score.cpuPts) text = ("[Congratulations! You're the winner!]");
+    else text = ("[The CPU is the winner. Better luck next time!]");
+    updateText(text);   
 }
 
-function showResults(){
-
+function updateText(str) {
+    console.log(str);
+    display.msg.textContent = str;
 }
 
+function reset(){
+    clearSelection();
+    score.playerPts = 0;
+    score.cpuPts = 0;
+}
 
-function updateSelection(btn) {
+function clearSelection(){
     rockBtn.classList.remove("selected");
     paperBtn.classList.remove("selected");
     scissorsBtn.classList.remove("selected");
-    selection = 0;
-    if (btn !=="clear") {
-        btn.classList.add("selected");
-        btn.getAttribute("data-option")
-    }
+    selection = null;
+}
+
+function updateSelection(btn) {
+    clearSelection();
+    btn.classList.add("selected");
+    selection = options[parseInt(btn.getAttribute("data-index"))];
 }
 
